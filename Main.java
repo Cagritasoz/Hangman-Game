2059a4c689ae1a694f578bebdaf1a54fb153ca9f
Cagritasoz <cagritasoz@gmail.com>
2025-02-22 15:46:45 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+public class Main {
+	public static void main(String[]args) {
+	try {	
+		
+		
+		
+		
+		
+		
+		
+		
+class Player {
+	
+
+
+
+
+
+
+
+
+
+
+class Game {
+	private String[] words = {"apple","banana","garden","blanket","astronaut","festival","labyrinth","dog","pineapple","lantern"};
+	int attemptNumber;
+	int difficultyNumber;
+	
+	public Game() {
+	}
+	
+	
+	
+	public String getRandomWord() {
+		Random random = new Random();
+		return words[random.nextInt(words.length)];
+	}
+	
+	
+		
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
