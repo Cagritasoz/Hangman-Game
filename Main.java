1252bc0fb2c4fcbf47a5eaf60a775740c746642a
Cagritasoz <cagritasoz@gmail.com>
2025-02-22 16:11:54 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,17 @@ import java.util.ArrayList;
 import java.util.Random;
 public class Main {
 	public static void main(String[]args) {
-	try {	
+		while(true) {
+			try {
+			System.out.println("Welcome to the Hangman Game!");
+			System.out.println("----------------------------");
+			System.out.println("Please choose the games difficulty");
+			System.out.println("-- Press 1 for easy mode (Maximum 8 attempts to fail)");
+			System.out.println("-- Press 2 for medium mode (Maximum 6 attempts to fail)");
+			System.out.println("-- Press 3 for hard mode (Maximum 4 attempts to fail)");
+			
+		
+			
 		
 		
 		
@@ -39,6 +49,19 @@ class Game {
 		return words[random.nextInt(words.length)];
 	}
 	
+	public void setdifficultyNumber(int difficultyNumber) {
+		this.difficultyNumber = difficultyNumber;
+	}
+	
+	public int getdifficultyNumber() {
+		return difficultyNumber;
+	}
+	public int getattemptNumber() {
+		return attemptNumber;
+	}
+	public void setattemptNumber(int attemptNumber) {
+		this.attemptNumber = attemptNumber;
+	}
 	
 		
 	
